521346cf0c5a5903594ebf75e588e3be849a856e
bahabarut <bahabarut2@gmail.com>
2024-04-06 09:36:23 +0300

    oyun tamamlandı

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var oyunDevamEdiyor = true;
-var interval = null;
-var hizSayac = 0;
+var yesilHizi = 10;
+var kirmiziHizi = 10;
+var yesilSkor = 0;
+var kirmiziSkor = 0;
+var interv = null;
 $(document).ready(function () {
     $("#Oyuncu1").attr("data-alan", AlanHesapla($("#Oyuncu1").width()));
     $("#Oyuncu2").attr("data-alan", AlanHesapla($("#Oyuncu2").width()));
-    console.log();
-    RandomElementlerEkle(5, 20);
+
     Baslat();
 });
 $(document).on("keydown", (e) => {
@@ -17,8 +19,8 @@ $(document).on("keyup", (e) => {
 });
 
 $("#SifirlaBtn").click(function () {
-    //location.reload();
-    CapHesapla("379.94");
+    location.reload();
+    //CapHesapla("379.94");
 });
 function RandomUret(min, max) {
     return Math.floor(min + Math.random() * max);
@@ -43,59 +45,60 @@ function RandomElementlerEkle(min, max) {
 }
 
 function Baslat() {
-    var inter = setInterval(function () {
+    RandomElementlerEkle(5, 20);
+    interv = setInterval(function () {
         var oyuncu1 = $("#Oyuncu1");
         var oyuncu2 = $("#Oyuncu2");
         //carpraz gitmek icin
-        if (tuslar[65].basili && tuslar[87].basili) YeniKonumHesapla(oyuncu1, "solyuk");
-        if (tuslar[65].basili && tuslar[83].basili) YeniKonumHesapla(oyuncu1, "solalt");
-        if (tuslar[68].basili && tuslar[87].basili) YeniKonumHesapla(oyuncu1, "sagyuk");
-        if (tuslar[68].basili && tuslar[83].basili) YeniKonumHesapla(oyuncu1, "sagalt");
+        if (tuslar[65].basili && tuslar[87].basili) YeniKonumHesapla(oyuncu1, "solyuk", yesilHizi);
+        if (tuslar[65].basili && tuslar[83].basili) YeniKonumHesapla(oyuncu1, "solalt", yesilHizi);
+        if (tuslar[68].basili && tuslar[87].basili) YeniKonumHesapla(oyuncu1, "sagyuk", yesilHizi);
+        if (tuslar[68].basili && tuslar[83].basili) YeniKonumHesapla(oyuncu1, "sagalt", yesilHizi);
 
-        if (tuslar[37].basili && tuslar[38].basili) YeniKonumHesapla(oyuncu2, "solyuk");
-        if (tuslar[37].basili && tuslar[40].basili) YeniKonumHesapla(oyuncu2, "solalt");
-        if (tuslar[39].basili && tuslar[38].basili) YeniKonumHesapla(oyuncu2, "sagyuk");
-        if (tuslar[39].basili && tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sagalt");
+        if (tuslar[37].basili && tuslar[38].basili) YeniKonumHesapla(oyuncu2, "solyuk", kirmiziHizi);
+        if (tuslar[37].basili && tuslar[40].basili) YeniKonumHesapla(oyuncu2, "solalt", kirmiziHizi);
+        if (tuslar[39].basili && tuslar[38].basili) YeniKonumHesapla(oyuncu2, "sagyuk", kirmiziHizi);
+        if (tuslar[39].basili && tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sagalt", kirmiziHizi);
         //tek yon gitmek icin
-        if (tuslar[65].basili && !tuslar[87].basili && !tuslar[83].basili) YeniKonumHesapla(oyuncu1, "sol");
-        if (tuslar[87].basili && !tuslar[68].basili && !tuslar[65].basili) YeniKonumHesapla(oyuncu1, "yuk");
-        if (tuslar[68].basili && !tuslar[83].basili && !tuslar[87].basili) YeniKonumHesapla(oyuncu1, "sag");
-        if (tuslar[83].basili && !tuslar[68].basili && !tuslar[65].basili) YeniKonumHesapla(oyuncu1, "alt");
+        if (tuslar[65].basili && !tuslar[87].basili && !tuslar[83].basili) YeniKonumHesapla(oyuncu1, "sol", yesilHizi);
+        if (tuslar[87].basili && !tuslar[68].basili && !tuslar[65].basili) YeniKonumHesapla(oyuncu1, "yuk", yesilHizi);
+        if (tuslar[68].basili && !tuslar[83].basili && !tuslar[87].basili) YeniKonumHesapla(oyuncu1, "sag", yesilHizi);
+        if (tuslar[83].basili && !tuslar[68].basili && !tuslar[65].basili) YeniKonumHesapla(oyuncu1, "alt", yesilHizi);
 
-        if (tuslar[37].basili && !tuslar[38].basili && !tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sol");
-        if (tuslar[38].basili && !tuslar[37].basili && !tuslar[39].basili) YeniKonumHesapla(oyuncu2, "yuk");
-        if (tuslar[39].basili && !tuslar[38].basili && !tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sag");
-        if (tuslar[40].basili && !tuslar[37].basili && !tuslar[39].basili) YeniKonumHesapla(oyuncu2, "alt");
-    }, 20);
+        if (tuslar[37].basili && !tuslar[38].basili && !tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sol", kirmiziHizi);
+        if (tuslar[38].basili && !tuslar[37].basili && !tuslar[39].basili) YeniKonumHesapla(oyuncu2, "yuk", kirmiziHizi);
+        if (tuslar[39].basili && !tuslar[38].basili && !tuslar[40].basili) YeniKonumHesapla(oyuncu2, "sag", kirmiziHizi);
+        if (tuslar[40].basili && !tuslar[37].basili && !tuslar[39].basili) YeniKonumHesapla(oyuncu2, "alt", kirmiziHizi);
+    }, 18);
 }
 
-function YeniKonumHesapla(Oyuncu, Yon) {
+function YeniKonumHesapla(Oyuncu, Yon, hiz) {
     var SinirGenislik = $("#OyunAlan").width() - Oyuncu.width();
     var SinirYukseklik = $("#OyunAlan").height() - Oyuncu.width();
     var OyuncuKonumLeft = Oyuncu.position().left;
     var OyuncuKonumTop = Oyuncu.position().top;
 
     switch (Yon) {
-        case "sol": OyuncuKonumLeft -= (5 - hizSayac); break;//sol a
-        case "yuk": OyuncuKonumTop -= (5 - hizSayac); break;//yukari w
-        case "sag": OyuncuKonumLeft += (5 - hizSayac); break;//sag d
-        case "alt": OyuncuKonumTop += (5 - hizSayac); break;//asagi s
+        case "sol": OyuncuKonumLeft -= hiz; break;//sol a
+        case "yuk": OyuncuKonumTop -= hiz; break;//yukari w
+        case "sag": OyuncuKonumLeft += hiz; break;//sag d
+        case "alt": OyuncuKonumTop += hiz; break;//asagi s
 
         case "solyuk":
-            OyuncuKonumLeft -= (5 - hizSayac);
-            OyuncuKonumTop -= (5 - hizSayac);
+            OyuncuKonumLeft -= hiz;
+            OyuncuKonumTop -= hiz;
             break;
         case "solalt":
-            OyuncuKonumLeft -= (5 - hizSayac);
-            OyuncuKonumTop += (5 - hizSayac);
+            OyuncuKonumLeft -= hiz;
+            OyuncuKonumTop += hiz;
             break;
         case "sagyuk":
-            OyuncuKonumLeft += (5 - hizSayac);
-            OyuncuKonumTop -= (5 - hizSayac);
+            OyuncuKonumLeft += hiz;
+            OyuncuKonumTop -= hiz;
             break;
         case "sagalt":
-            OyuncuKonumLeft += (5 - hizSayac);
-            OyuncuKonumTop += (5 - hizSayac);
+            OyuncuKonumLeft += hiz;
+            OyuncuKonumTop += hiz;
             break;
     }
 
@@ -134,24 +137,47 @@ function ToplarCakisti(oyuncu) {
 
         var dikeyKosul = oyuncuK.top < elK.bottom && oyuncuK.bottom > elK.top;
         var yatayKosul = oyuncuK.left < elK.right && oyuncuK.right > elK.left;
-
-        if (dikeyKosul && yatayKosul && !($(el).is(".Oyuncular"))) {
+        var kosul = dikeyKosul && yatayKosul;
+        if (kosul && !($(el).is(".Oyuncular"))) {
             var yeniAlan = AlanHesapla($(el).width());
             $(oyuncu).css("width", CapHesapla(yeniAlan) + $(oyuncu).width() + "px");
             $(oyuncu).css("height", CapHesapla(yeniAlan) + $(oyuncu).height() + "px");
             $(el).remove();
             RandomElementlerEkle(1, 1);
+            if ($(oyuncu).is("#Oyuncu1"))
+                yesilHizi -= CapHesapla(yeniAlan).toFixed(2) / 45;
+            if ($(oyuncu).is("#Oyuncu2"))
+                kirmiziHizi -= CapHesapla(yeniAlan).toFixed(2) / 45;
         }
-        if (dikeyKosul && yatayKosul && $(el).is(".Oyuncular") && $(oyuncu).is(".Oyuncular")) {
-
+        if (kosul && ($(oyuncu).is("#Oyuncu1") && $(el).is("#Oyuncu2") || $(oyuncu).is("#Oyuncu2") && $(el).is("#Oyuncu1"))) {
+            if ($("#Oyuncu1").width() > $("#Oyuncu2").width()) {
+                yesilSkor += 1;
+                SkorGuncelle("#yesilSkor", yesilSkor, "Yeþil", "green");
+            }
+            else {
+                kirmiziSkor += 1;
+                SkorGuncelle("#kirimiziSkor", kirmiziSkor, "Kýrmýzý", "red");
+            }
+            OynamayaDevamEt();
+            clearInterval(interv);
+            Baslat();
+            setTimeout(function () { $("#sonuc").empty(); }, 2000);
         }
     });
+}
+function SkorGuncelle(skorElm, skor, kazananRenk, color) {
+    $(skorElm).text(skor.toString());
+    $("#sonuc").text(kazananRenk + " Kazandý");
+    $("#sonuc").css("color", color);
 }
 function AlanHesapla(cap) {
     return (((cap / 2) ** 2) * 3.14).toFixed(2);
 }
 function CapHesapla(alan) {
     var cap = Math.sqrt((alan / 3.14));
-    console.log(cap);
     return cap;
+}
+function OynamayaDevamEt() {
+    $("#OyunAlan").empty();
+    $("#OyunAlan").html("<span id='Oyuncu1' class='Oyuncular elem'></span> <span id='Oyuncu2' class='Oyuncular elem'></span>");
 }
